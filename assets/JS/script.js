a4917db1fcc0bc9543d572643ddb74338b14fbeb
Mundino Hamure <mundinoabete@gmail.com>
2024-08-02 21:37:47 +0000

    Add complete and delete button function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,4 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
       taskInput.value = "";
     }
   });
+  function addTask(taskText) {
+    const taskItem = document.createElement("li");
+    taskItem.className = "task-item";
+    const taskSpan = document.createElement("span");
+    taskSpan.textContent = taskText;
+  }
+
+  const completeBtn = document.createElement("button");
+  completeBtn.className = "complete-btn";
+  completeBtn.textContent = "Complete";
+  completeBtn.addEventListener("click", () => {
+    taskItem.classList.toggle("completed");
+  });
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete-btn";
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", () => {
+    taskItem.remove();
+  });
+  
 });
